d7267291593717999dcce1e88b9dbe190a4431ce
Jason Deng <jdeng1234@gmail.com>
2016-10-09 04:49:04 -0400

    doesnt matter

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.post('/post/incoming', function(req, res) {
             }
             , function (e,r,b) {
               var type    = r.headers["content-type"];
-              var prefix  = "data:" + type + ";base64,";
+              var prefix  = "data:" + "image" + ";base64,";
               var base64  = new Buffer(b, 'binary').toString('base64');
               var b64content = prefix + base64;
 
@@ -202,7 +202,7 @@ app.post('/post/incoming', function(req, res) {
           res.end(resp.toString());
     })
 
-  
+
 })
 
 

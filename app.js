895e67b56c8310982173443e27466ce87005d306
Jason Deng <jdeng1234@gmail.com>
2016-10-09 00:42:48 -0400

    sada

--- app.js
+++ app.js
@@ -117,7 +117,9 @@ app.post('/post/incoming', function(req,res) {
 
   var resp = new twilio.TwimlResponse();
    resp.message('ahoy hoy! Testing Twilio and node.js');
-   console.log(resp);
+   console.log(res.body);
+   console.log(req.body);
+
    //Render the TwiML document using "toString"
    res.writeHead(200, {
        'Content-Type':'text/xml'

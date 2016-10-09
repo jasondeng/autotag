a16b432789187a7794a65718e4a7d8165f51add1
Jason Deng <jdeng1234@gmail.com>
2016-10-09 02:59:01 -0400

    MIGHT BE FINISHED

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var qs = require('querystring');
-var Twit = require('twit')
+var Twit = require('twit');
+var fs = require('fs');
 
 var config;
 try {
   config = require('./config');
-}
-catch(e) {
+} catch (e) {
   console.log('config not found');
   config = process.env;
 }
@@ -45,11 +45,11 @@ var User = require("./models/user");
 
 app.post('/post/phonenum', function(req, res) {
   var data = req.body;
-  User.findOneAndUpdate({access_token: data.token}, {phone_number: data.phone_number}, {new: true}, function(err, doc) {
+  User.findOneAndUpdate({ access_token: data.token }, { phone_number: data.phone_number }, { new: true }, function(err, doc) {
     if (err)
       return res.send(err);
     if (!doc)
-      return res.status(404).send({message: "Not found"});
+      return res.status(404).send({ message: "Not found" });
     return res.send(doc);
   })
   console.log(data);
@@ -89,12 +89,12 @@ app.post('/auth/twitter', function(req, res) {
 
       accessToken = qs.parse(accessToken);
       console.log("ACCESSTOKEN ", accessToken);
-/*
-      consumer_key: config.TWITTER_KEY,
-      consumer_secret: config.TWITTER_SECRET,
-      access_token: accessToken.oauth_token,
-      access_token_secret: accessToken.oauth_token_secret,
-*/
+      /*
+            consumer_key: config.TWITTER_KEY,
+            consumer_secret: config.TWITTER_SECRET,
+            access_token: accessToken.oauth_token,
+            access_token_secret: accessToken.oauth_token_secret,
+      */
       var user = new User({
         twitter: accessToken.screen_name,
         access_token: accessToken.oauth_token,
@@ -102,12 +102,12 @@ app.post('/auth/twitter', function(req, res) {
       })
 
       user.save(function(err) {
-        if(err) {
+        if (err) {
           console.log(err);
           return res.status(400).send(err);
         }
         console.log('asdas');
-        return res.send({token: user.access_token});
+        return res.send({ token: user.access_token });
       });
     });
   }
@@ -118,63 +118,105 @@ var TWILIO_AUTH_TOKEN = config.TWILIO_AUTH_TOKEN;
 var twilio = require('twilio');
 var client = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-app.post('/post/incoming', function(req,res) {
+app.post('/post/incoming', function(req, res) {
 
-  var resp = new twilio.TwimlResponse();
-   resp.message('We have recieved your image!');
-   console.log(req.body);
-
-   request('https://api.clarifai.com/v1/tag?url=' + req.body.MediaUrl0 + '&access_token=' + config.CLARIFAI_TOKEN, function(error, response, body) {
-     if (!error && response.statusCode == 200) {
-       var data = JSON.parse(body).results[0].result.tag.classes;
-
-/*
-       var T = new Twit({
-         consumer_key: config.TWITTER_KEY,
-         consumer_secret: config.TWITTER_SECRET,
-         access_token: accessToken.oauth_token,
-         access_token_secret: accessToken.oauth_token_secret,
-       });*/
-     }
-     //return res.send(response.body);
-   })
-   //Render the TwiML document using "toString"
-   res.writeHead(200, {
-       'Content-Type':'text/xml'
-   });
-
-   res.end(resp.toString());
-
-});
+    var resp = new twilio.TwimlResponse();
+    resp.message('We have recieved your image!');
 
 
-   /*
+    request('https://api.clarifai.com/v1/tag?url=' + req.body.MediaUrl0 + '&access_token=' + config.CLARIFAI_TOKEN, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body).results[0].result.tag.classes;
 
-   var T = new Twit({
-     consumer_key: config.TWITTER_KEY,
-     consumer_secret: config.TWITTER_SECRET,
-     access_token: accessToken.oauth_token,
-     access_token_secret: accessToken.oauth_token_secret,
-   });
-*/
-/*
-   // first we must post the media to Twitter
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-// now we can assign alt text to the media, for use by screen readers and
-// other text-based presentations and interpreters
-var mediaIdStr = data.media_id_string;
-var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+        var from_number = req.body.From;
+        from_number = from_number.slice(2);
 
-T.post('media/metadata/create', meta_params, function (err, data, response) {
- if (!err) {
-   // now we can reference the media and post a tweet (media will attach to the tweet)
-   var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+        User.findOne({ phone_number: from_number }, function(err, user) {
+          if (err)
+            res.send(err);
+          if (user) {
+            var T = new Twit({
+              consumer_key: config.TWITTER_KEY,
+              consumer_secret: config.TWITTER_SECRET,
+              access_token: user.access_token,
+              access_token_secret: user.access_token_secret,
+            });
 
-   T.post('statuses/update', params, function (err, data, response) {
-     console.log(data)
-   })
- }*/
+            var b64content = fs.readFileSync(req.body.MediaUrl0, { encoding: 'base64' })
+
+
+            T.post('media/upload', { media_data: b64content }, function(err, data, response) {
+              // now we can assign alt text to the media, for use by screen readers and
+              // other text-based presentations and interpreters
+              var mediaIdStr = data.media_id_string;
+              var altText = "This image was submitted via Twilio."
+              var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+              T.post('media/metadata/create', meta_params, function(err, data, response) {
+                if (!err) {
+                  // now we can reference the media and post a tweet (media will attach to the tweet)
+
+                  var newData = data.join(" #");
+                  newData = '#' + newData;
+
+                  if (newData.length > 140) {
+                    for (var i = 139; i > 0; i--) {
+                      if (newData[i] === "#") {
+                        newData = newData.slice(0, i)
+                        break;
+                      }
+                    }
+                  }
+                  var params = {
+                    status: newData,
+                    media_ids: [mediaIdStr]
+                  }
+
+                  T.post('statuses/update', params, function(err, data, response) {
+                    console.log(data)
+                  })
+                }
+              })
+            })
+          }
+        });
+      }
+    })
+  })
+  /*
+
+       }
+       //return res.send(response.body);
+     })
+     //Render the TwiML document using "toString"
+     res.writeHead(200, {
+         'Content-Type':'text/xml'
+     });
+
+     res.end(resp.toString());
+
+  });
+
+
+
+  /*
+     // first we must post the media to Twitter
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreters
+  var mediaIdStr = data.media_id_string;
+  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+  T.post('media/metadata/create', meta_params, function (err, data, response) {
+   if (!err) {
+     // now we can reference the media and post a tweet (media will attach to the tweet)
+     var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+
+     T.post('statuses/update', params, function (err, data, response) {
+       console.log(data)
+     })
+   }*/
 
 
 //var client = require('twilio')(config.ACCOUNT_SID, config.TWILIO_AUTH_TOKEN);
